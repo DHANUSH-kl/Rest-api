23f7ffc0a919ebd1395255b966d43b2c6c9edf37
Dhanush KL <dhanushchandu1232@gmail.com>
2023-11-28 21:45:44 +0530

    update and delete functions

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express()
 const port = 8080
 const path = require("path")
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
+
+
 app.use(express.urlencoded({extended:true}))
-app.set("views",path.join(__dirname,"views"))
 app.use(express.static(path.join(__dirname,"public")))
+app.use(methodOverride('_method'));
+
+app.set("views",path.join(__dirname,"views"))
 
 app.set("view engine", "ejs")
 
@@ -50,4 +55,25 @@ app.get("/post/:id",(req,res)=> {
     let {id} = req.params;
     let post=posts.find((p)=>id===p.id)
     res.render("show",{post})
+})
+
+app.patch("/post/:id",(req,res)=> {
+    let {id}=req.params;
+    let newContent=req.body.content;
+    let post = posts.find((p)=>id===p.id)
+    post.content = newContent;
+    res.redirect("/post")
+})
+
+app.get("/post/:id/edit",(req,res)=> {
+    let {id}=req.params;
+    let post = posts.find((p)=> id===p.id);
+    res.render("edit",{post})
+})
+
+app.delete("/post/:id",(req,res)=> {
+    let {id}=req.params;
+    posts = posts.filter((p)=> id!==p.id);
+    res.redirect("/post")
+
 })
